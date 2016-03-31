3e8b506e434d2242f4695490521af6a1140835e7
Oscar Xavier Guerrero <oscarxavier.ox@gmail.com>
2016-03-30 20:47:42 -0500

    Added MDynn, update ediff

--- analysisalgs.py
+++ analysisalgs.py
@@ -9,6 +9,7 @@ class Ediff(object):
         self.area = parts.pop('area', False)
         # calc
         self._get_data_from_parts(parts)
+        self.ads = 'ads' in parts
         # run
         if isinstance(self.complet, Check):
             res = {self.complet.nam: {'Ediff': self._get_energy_diff(self.complet),
@@ -44,6 +45,10 @@ class Ediff(object):
             parts_energy += relE
         #
         res = complet.F - parts_energy
+        if self.ads:
+            ads_ratio = complet.elements.get(self.ads_elm) / self.ads_num
+            if self.v: print 'ads_ratio:', ads_ratio
+            res /= ads_ratio
         return res / area
 
     def _get_data_from_parts(self, parts):
@@ -61,7 +66,10 @@ class Ediff(object):
                     res[nam] = {'energy': tmp.F,
                                 'ratio': {},
                                 'relE': {}}
-                    if v: print "fin read part"
+                    if v: 
+                        print "\n".join(["  {}: {}".format(k, v)
+                                         for k,v in res.iteritems()])
+                        print "fin read part"
             elif cat == 'bulk' or cat == 'ads':
                 if v: print "init read part", path
                 tmp = Check(path, v=v)
@@ -69,12 +77,17 @@ class Ediff(object):
                 res[nam] = {'energy': tmp.F,
                             'ratio': next(tmp.elements.iteritems()),
                             'relE': {}}
-                if v: print "fin read part"
+                if cat == 'ads':
+                    self.ads_elm, self.ads_num = res[nam]['ratio']
+                if v: 
+                    print "\n".join(["  {}: {}".format(k, v)
+                                     for k,v in res.iteritems()])
+                    print "fin read part"
         self.parts = res
 
     def __str__(self):
         res = ""
-        for nam, val in self.res.iteritems():
+        for nam, val in sorted(self.res.items()):
             res += "{:12}: {:11.5f}\n".format(nam, val['Ediff'])
             if self.rep:
                 res += "  {:10}: {:11.5f}\n".format(nam, val['complet'])
@@ -83,5 +96,26 @@ class Ediff(object):
                                                         part['relE'][nam])
                 if self.area:
                     res += "  {:10}: {:11.5f}\n".format("area", val['area'])
-            res += "\n"
+            if self.rep: res += "\n"
         return res[:-2]
+#..
+
+def MDynn(folder):
+    calcs = folder.calcs
+
+    res = str(calcs[0])
+    res.replace("io_step", "   time")
+    prev = int(res.split("\n")[-1].split()[0])
+    
+    for i in xrange(1, len(calcs)):
+        lines = str(calcs[i]).split("\n")[1:]
+        for i_line in xrange(len(lines)):
+            line = lines[i_line]
+            orig = line.split()[0]
+            new_val = int(orig) + prev
+            lines[i_line] = "{:>8}".format(new_val) + line[8:]
+        prev = lines[-1].split()[0]
+        res += "\n" + "\n".join(lines)    
+        
+    return res
+    
