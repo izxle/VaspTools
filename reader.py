1503131f5684b0fcbb541c842b4711f9d291b9d2
Oscar Xavier Guerrero <oscarxavier.ox@gmail.com>
2016-12-28 16:08:06 -0600

    Python 3 compatibility and cleaned code

--- reader.py
+++ reader.py
@@ -1,6 +1,7 @@
 from re import compile
 from os import path, walk, listdir
 from numpy import array, cross
+from myfunctions import printv
 
 class Check(object):
     def __init__(self, f_path, reps=[], subdir='',
@@ -18,9 +19,9 @@ class Check(object):
         self.reps = reps
         self.to_float = ['F', 'F_n', 'E0', 'E', 'Temp', 'area', 'm', 'dE', 't']
 
-        if self.v>1: print '{0}in calc'.format(pad)
+        if self.v>1: printv(pad, 'in calc')
         self.pad = pad + "  "
-        if self.v: print '{0}f_path: {1}'.format(self.pad, self.f_path)
+        if self.v: printv(pad, 'f_path: ', self.f_path)
 
         # get n_atoms
         self._readPOSCAR()
@@ -28,33 +29,34 @@ class Check(object):
         self._readOSZICAR()
         # get time data
         self._readOUTCAR()
-        if self.v>1: print '{0}end calc'.format(self.pad[:-2])
+        if self.v>1: printv(self.pad[:-2], 'end calc')
 
     def _readPOSCAR(self):
         # TODO: ignore comments
         try:
             nams = listdir(self.f_path)
-        except OSError, e:
-            raise IOError(e if self.v else '{} does not exist'.format(self.subdir))
+        except OSError as e:
+            printv(self.subdir,  'does not exist')
+            raise IOError(e)
         for nam in ['POSCAR', 'CONTCAR']:
             if nam in nams: break
             raise IOError('No structure files detected')
         path = self.getPath(nam)
-        if self.v: print '{0}reading structure'.format(self.pad)
+        if self.v: printv(self.pad, 'reading structure')
         with open(path, 'r') as f:
             # TODO: read more data
-            # with ASE
+            # TODO: do with ASE
             lines =  f.readlines()
-            if not lines: raise IOError('No POSCAR found')
+            if not lines: raise IOError('Empty file')
             a = array(map(float, lines[2].split()))
             b = array(map(float, lines[3].split()))
             elms = lines[5].split()
         nums = map(int, lines[6].split())
         n_atoms = sum(nums)
-        if self.v: print "{0}n_atoms: {1}".format(self.pad, n_atoms)
+        if self.v: printv(self.pad, "n_atoms: ", n_atoms)
 
         self.area = cross(a, b)[2]
-        self.elements = {elms[i]: nums[i] for i in range(len(elms))}
+        self.elements = {elm: nums[i] for i, elm in enumerate(elms)}
         self.n_atoms = n_atoms
 
     def _readOSZICAR(self):
@@ -70,28 +72,28 @@ class Check(object):
         rex += '(?:SP=\s*(?P<SP>[+\-.0-9E]+)\s*)?' # thermostat PE
         rex += '(?:SK=\s*(?P<SK>[+\-.0-9E]+)\s*)?' # thermostat KE
         rex += '(?:mag=\s*(?P<m>[+\-.0-9E]+)\s*)?' # magnetic
-        if self.v>2: print "{0}regex: {1}".format(self.pad, rex)
+        if self.v>2: printv(self.pad, "regex: ", rex)
         regex = compile(rex)
         # read file
         try:
             self._search('OSZICAR', regex)
         # TODO: distinguish between no file and no match?
         except IOError:
-            if self.v: print "ERROR, no matches found."
+            if self.v: printv("ERROR, no matches found.")
             self._set_blank()
 
     def _readOUTCAR(self):
         # regex to get data
         # TODO: read more data
         rex = 'Total CPU time used \(sec\):\s*(?P<t>[+\-.0-9E]+)'
-        if self.v>3: print "{0}regex: {1}".format(self.pad, rex)
+        if self.v>3: printv(self.pad, "regex: ", rex)
         regex = compile(rex)
         # read file
         try:
             self._search('OUTCAR', regex)
         # TODO: distinguish between no file and no match?
         except IOError:
-             if self.v: print "ERROR, no matches found."
+             if self.v: printv("ERROR, no matches found.")
              self.t = 0
 
     def _search(self, nam, regex):
@@ -100,24 +102,19 @@ class Check(object):
         path = self.getPath(nam)
         with open(path, 'r') as f:
             txt = f.read()
-            if v: print "{0}.. {1} loaded".format(pad, nam)
-        matches = [{k: v for k, v in m.groupdict().iteritems() if not v is None}
+            if v: printv(pad, ".. {1} loaded".format(nam))
+        matches = [{k: v for k, v in m.groupdict().iteritems() if v is not None}
                    for m in regex.finditer(txt)]
         if not matches:
             # TODO: distinguish between no file and no match?
             raise IOError
             #raise Exception('No matches found.')
-        if v: print '{0}{1} matches found'.format(pad, len(matches))
-        if v>2:
-            print '{0}matches:'.format(pad)
-            pad += "  "
-            for m in matches:
-                for k, v in m.iteritems():
-                    print '{0}{k}: {v}'.format(pad, k=k, v=v)
-                print "-----"
+        if v: printv(pad, len(matches), ' matches found')
+        if v>2: printv(pad, 'matches:\n', matches, "\n-----")
+        # TODO: handle possible non-numeric values
         last_match = {k: float(val) if k in self.to_float else int(val)
                       for k, val in  matches[-1].iteritems()}
-        if v>3: print "last_match:", last_match
+        if v>3: printv("last_match:", last_match)
         # store last match
         self.update(**last_match)
         # store more matches
@@ -192,8 +189,8 @@ class Folder(object):
         self.pad = pad
         self.ignore = i
 
-        if self.v>1: print '{0}in compare'.format(pad)
-        if self.v: print '{0}f_path: {1}'.format(pad, self.f_path)
+        if self.v>1: printv(pad, 'in compare')
+        if self.v: printv(pad, 'f_path: ', self.f_path)
 
         self._run()
 
@@ -202,28 +199,28 @@ class Folder(object):
         # get dirs
         dirs = next(walk(self.f_path))[1]
         # ignore templates
-        for dir in self.ignore:
-            if dir in dirs:
-                dirs.remove(dir)
+        for d in self.ignore:
+            if d in dirs:
+                d.remove(dir)
             else:
-                print "{} does not exist".format(dir)
+                printv(d, " does not exist")
         # sort list
         try:
             # mutate list
             dirs.sort(key=float)
         except ValueError:
             dirs = sorted(dirs)
-        if self.v: print "{0}dirs: {1}".format(self.pad, dirs)
+        if self.v: printv(self.pad, "dirs: ", dirs)
         # get data
         for subdir in dirs:
-            if self.v: print "{0}in {1}".format(self.pad, subdir)
+            if self.v: printv(self.pad, "in ", subdir)
             f_path = self.getPath(subdir, self.subdir)
             try:
                 calc = Check(f_path=f_path, pad=self.pad+'  ',
                              v=self.v, subdir=self.subdir)
                 self.calcs.append(calc)
-            except IOError, e:
-                print "{}: {}".format(subdir, e)
+            except IOError as e:
+                printv("{}: {}".format(subdir, e))
         self._formatData()
 
     def getPath(self, nam, subdir=''):
@@ -259,9 +256,9 @@ class Folder(object):
             pos_data = data[data > 0.0]
             if len(pos_data)==0:
                 if rep == 'F' or rep == 'F_n':
-                    print "ERROR: all F >= 0."
+                    printv("ERROR: all F >= 0.")
                 elif rep=='t':
-                    print "ERROR: all t <= 0."
+                    printv("ERROR: all t <= 0.")
                 # TODO: display positive values for F?
                 maxV = 0
                 minV = 0
@@ -272,7 +269,7 @@ class Folder(object):
             relV = (data - minV) * 62 / (maxV - minV)
             relV.clip(0)
 
-            if self.v>1: print 'data to print:', data
+            if self.v>1: printv('data to print: ', data)
 
             res += '  nam | {0:>9} |\n'.format(header)
             res += '\n'.join(['{0:>5} | {1:9.5f} |{2}'
