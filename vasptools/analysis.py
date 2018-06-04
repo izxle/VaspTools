72a453a182cabd10d9b26f759c2c5ddfb02dc2ca
izxle <oscarxavier.ox@gmail.com>
2018-06-04 13:35:03 -0500

    created objects for adsorption and surface energies

--- vasptools/analysis.py
+++ vasptools/analysis.py
@@ -1,23 +1,46 @@
 
-from .reader import read, hasdirs
-from .report import ReportSingle, ReportCompare, ReportAdsorption, ReportSurface
+from .reader import read
+from .report import ReportCompare, ReportCompareAdsorption
+from .report import ReportSingle, ReportSingleAdsorption, ReportSimpleSurface
 from .result import Result
 
 
 def generate_report(results: list, ads: bool=False, surf_en: bool=False, reps=None, subdir=''):
 
     if isinstance(results, Result):
-        report = ReportSingle(results, reps=reps)
+        if ads:
+            slab = read(directory=ads.slab)
+            adsorbate = read(directory=ads.adsorbate)
+            report = ReportSingleAdsorption(whole=results,
+                                            slab=slab,
+                                            ads=adsorbate)
+        elif surf_en:
+            bulk = read(directory=surf_en.bulk)
+            report = ReportSimpleSurface(slab=results,
+                                         bulk=bulk,
+                                         area=surf_en.area)
+        else:
+            report = ReportSingle(results, reps=reps)
+
+
+
     elif isinstance(results, list):
         if not all(isinstance(r, Result) for r in results):
             raise ValueError()
 
         if ads:
-            report = ReportAdsorption(results)
-        elif surf_en:
-            report = ReportSurface(results)
+            slab = read(directory=ads.slab)
+            adsorbate = read(directory=ads.adsorbate)
+            report = ReportCompareAdsorption(results=results,
+                                             slab=slab,
+                                             ads=adsorbate,
+                                             subdir=subdir)
+        # elif surf_en:
+        #     report = ReportSurface(results)
         else:
             report = ReportCompare(results, reps=reps, subdir=subdir)
+        # else:
+        #     raise NotImplementedError()
 
     else:
         raise ValueError()
--- vasptools/report.py
+++ vasptools/report.py
@@ -1,9 +1,12 @@
 from .result import Result
 import numpy as np
 from os import path
+from collections import Counter
 
 
 class Report:
+    _float_format = '9.4f'
+    _name_len = 11
     def __init__(self, subdir=''):
         self.subdir = subdir
 
@@ -18,9 +21,9 @@ class ReportSingle(Report):
         r = self.result
 
         if self.reps is None:
-            oszicar = str(r.oszicar)
+            results = str(r)
         else:
-            oszicar = r.oszicar.report(self.reps)
+            results = r.report(self.reps)
 
         if isinstance(r.time, float):
             time = f'time: {r.time/3600:8.4f} h | {r.time:9.3f} s'
@@ -29,14 +32,14 @@ class ReportSingle(Report):
 
         text = (
             f'{r.name}\n'
-            f'{oszicar}\n'
+            f'{results}\n'
             f'{time}')
 
         return text
 
 
 class ReportCompare(Report):
-    _line_lenght = 62
+    _line_len = 62
     def __init__(self, results, subdir: str='', reps=('F', 'time')):
         super().__init__(subdir=subdir)
         self.results = results
@@ -64,13 +67,13 @@ class ReportCompare(Report):
                 if len(vals) > 0:
                     max_val = max(vals)
                     min_val = min(vals)
-                    relative_values = (vals - min_val) * self._line_lenght / (max_val - min_val)
-                relative_values[nan_mask] = self._line_lenght
+                    relative_values = (vals - min_val) * self._line_len / (max_val - min_val)
+                relative_values[nan_mask] = self._line_len
 
             else:
                 max_val = max(values)
                 min_val = min(values)
-                relative_values = (values - min_val) * self._line_lenght / (max_val - min_val)
+                relative_values = (values - min_val) * self._line_len / (max_val - min_val)
 
             text += f' name       | {rep:9} |\n'
             for res, rval in zip(self.results, relative_values):
@@ -82,16 +85,105 @@ class ReportCompare(Report):
                 name = path.basename(name)
                 text += (f'{name[:11]:11} | {res.get(rep):9.3f} |'
                          f'{"*" * int(round(rval))}\n')
-            text += f"{'':25}{'':->{self._line_lenght}}>\n"
+            text += f"{'':25}{'':->{self._line_len}}>\n"
         return text
 
 
-class ReportAdsorption(Report):
-    def __init__(self, subdir):
+class ReportSingleAdsorption(Report):
+    def __init__(self, whole, slab, ads):
+        super().__init__()
+        self.whole = whole
+        self.slab = slab
+        self.ads = ads
+
+        self.name = whole.name
+
+        # use first element to calculate ratio
+        element = ads.atoms.get_chemical_symbols()[0]
+        self.ads_element = element
+        n = whole.elements[element] / ads.elements[element]
+        self.ratio = n
+        ads_en = whole.E0 - slab.E0 - n * ads.E0
+        self.ads_en = ads_en
+
+    def __str__(self):
+        text = (
+            f'{self.name}\n'
+            f'adsorption energy: {self.ads_en:{self._float_format}}\n'
+            f'  slab + ads       {self.whole.E0:{self._float_format}}\n'
+            f'  slab             {self.slab.E0:{self._float_format}}\n'
+            f'{self.ratio:3} x ads ({self.ads_element:>2})     {self.ratio * self.ads.E0:{self._float_format}}\n'
+            f'  ads              {self.ads.E0:{self._float_format}}\n'
+        )
+        return text
+
+
+class ReportCompareAdsorption(Report):
+    def __init__(self, results, slab, ads, subdir=''):
         super().__init__(subdir)
+        self.slab = slab
+        self.ads = ads
+        self.results = results
+
+        ads_energies = dict()
+        for whole in results:
+            report = ReportSingleAdsorption(whole=whole,
+                                            slab=slab,
+                                            ads=ads)
+            # name handling
+            root, basename = path.split(report.name)
+            if basename == self.subdir:
+                name = root
+            else:
+                name = report.name
+            name = path.basename(name)
+            ads_energies[name] = report
+
+        self.ads_energies = ads_energies
+
+    def __str__(self):
+        text = 'Adsorption Energies\n'
+
+        for name, report in self.ads_energies.items():
+            text += f'{name[:self._name_len]:{self._name_len}} {report.ads_en:{self._float_format}}\n'
+
+        return text
 
 
-class ReportSurface(Report):
-    def __init__(self, subdir):
+class ReportSimpleSurface(Report):
+    def __init__(self, slab, bulk, area=None):
+        super().__init__()
+        self.slab = slab
+        self.bulk = bulk
+        if area:
+            self.area = area
+        else:
+            self.area = slab.area
+
+        self.name = slab.name
+
+        element = bulk.atoms.get_chemical_symbols()[0]
+        self.bulk_element = element
+        n = slab.elements[element] / bulk.elements[element]
+        self.ratio = n
+
+        self.surface_energy = (slab.E0 - n * bulk.E0) / (2 * self.area)
+
+    def __str__(self):
+        text = (
+            f'{self.name}\n'
+            f'surface energy:    {self.surface_energy:{self._float_format}}\n'
+            f'  area             {self.area:{self._float_format}}\n'
+            f'  slab             {self.slab.E0:{self._float_format}}\n'
+            f'{self.ratio:3} x bulk ({self.bulk_element:>2})    {self.ratio * self.bulk.E0:{self._float_format}}\n'
+            f'  bulk             {self.bulk.E0:{self._float_format}}\n'
+        )
+        return text
+
+
+class ReportCompareSurface(Report):
+    def __init__(self, results, bulk, subdir=''):
         super().__init__(subdir)
-
+        self.results = results
+        self.bulk = bulk
+        # TODO: finish ReportCompareSurface
