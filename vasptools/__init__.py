d16efb6d84cc243f5e01fa6dfd7e40784a16f1b4
izxle <oscarxavier.ox@gmail.com>
2018-07-02 11:22:08 -0500

    added functions to main package

--- vasptools/__init__.py
+++ vasptools/__init__.py
@@ -1,6 +1,7 @@
 
 from .reader import read, hasdirs
 from .analysis import generate_report
+from .tools import fix_layers, set_tags, invert_z, correct_z, tag_layers
 
 
 __all__ = ['result', 'read', 'hasdirs', 'generate_report']
