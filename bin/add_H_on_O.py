343973894bbc75680d43ecf3d4baf322c8a6d84c
izxle <oscarxavier.ox@gmail.com>
2020-02-09 18:06:30 -0600

    added support for multiple O atoms

--- bin/add_H_on_O.py
+++ bin/add_H_on_O.py
@@ -4,11 +4,15 @@ from ase.io import read
 from ase import Atom
 
 slabO = read('POSCAR')
-# get index of first O atom
-index_O = next(a.index for a in slabO if a.symbol == 'O')
-O = slabO[index_O]
-pos_H = O.position + (0, 0, 0.987)
-H = Atom('H', pos_H)
-slabOH = slabO + H
+
+slabOH = slabO.copy()
+index_O = (a.index for a in slabO if a.symbol == 'O')
+for ox in index_O:
+    O = slabO[ox]
+    pos_H = O.position + (0, 0, 0.987)
+    H = Atom('H', pos_H)
+    slabOH.extend(H)
+
 slabOH.write('POSCAR', format='vasp', vasp5=True, sort=True, direct=True)
+
 
